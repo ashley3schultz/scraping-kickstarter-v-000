875d211e546f7783c1547e9c5716c51bacbcc8a5
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-14 15:23:21 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,9 @@ def create_project_hash
   list = kickstarter.css("li.project.grid_4")
   list.each {|project|
     title = project.css("h2.bbcard_name strong a").text
-    image = project.css("div.project-thumbnail a img").attribute("src").value}
+    image = project.css("div.project-thumbnail a img").attribute("src").value
+    description = 
+  }
 end
 create_project_hash
 
