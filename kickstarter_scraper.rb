35113211877773ab2be4efa18d8744e017f9914c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-14 15:19:33 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
   list = kickstarter.css("li.project.grid_4")
-  list.each {|project| 
+  list.each {|project|
     title = project.css("h2.bbcard_name strong a").text
     image = project.css("div.project-thumbnail a img").attribute("src").value}
 end
