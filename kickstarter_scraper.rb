0ba9a2ab231621d971e404c1d13358809cdf6a03
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-14 16:00:55 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,7 +18,7 @@ def create_project_hash
       
       project.css("ls.first funded strong").text
       
-      project.css("ul.project-stats strong").text
+      project.css("ul.project-stats strong").text.comp("$2*")
   }
 end
 create_project_hash
